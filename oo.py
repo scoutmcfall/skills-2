f7fbbbfa87278755114c44d8e84c6c2d9f7770c6
Scout McFall <scoutmknz@gmail.com>
2021-09-13 00:30:54 +0000

    clean up extra unused code

--- oo.py
+++ oo.py
@@ -61,16 +61,13 @@ class Library: #doesn't need to inherit from book because everything in book is 
 
     def __init__(self):
         """Create a library."""
-        #use super to get the title and author methods from Book class?
-        #super().__init__()
-        #should the library be a dictionary of author, title k/v pairs?
+        
         self.books = []
         
 
     def create_and_add_book(self, title, author):
         """Create a Book and add it to the library's list of books."""
         #take in title and author, instantiate a book
-        #use super to get the book class?
         
         book = Book(title, author)
         #add book to the library's list of books
@@ -81,30 +78,12 @@ class Library: #doesn't need to inherit from book because everything in book is 
     def find_books_by_author(self, author):
         """Return a list of books by the given author."""
         books_by_author = []
-        #take in an author and return a list of all books written by that author
-        #books_by_author.append(getattr(self, author,[]))
-        #super().__init__(self, author)
+        
         for book in self.books:
             if book.author == author:
                 books_by_author.append(book)#is asking for the book object, not just the title
         return books_by_author
-            # else:
-            #     continue
-      
-        # for item in self.books:
-        #     if author in item:
-        #         books_by_author.append(item[1:])
-        #     else:
-        #         continue
-        # if author in self.books:
-        #    books_by_author.append(self.title)
-        # else:
-        #     return books_by_author
-        
-        # for self.book in self.books:
-        #     if self.author in self.books:
-        #         books_by_author.append(self.title)
-        #if no books, return empty list
+           
         
         # TODO: replace this with your code
 
